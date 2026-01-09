1a44aa0cb83b92809d34271635f4728cbaa35f5e
ghada_jarrar <ghada.jarrarr@gmail.com>
2026-01-09 21:22:42 +0100

    carvexx

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,28 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/user.module';
 import { CarsModule } from './cars/cars.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { OffersModule } from './offres/offres.module';
-import { ConfigModule } from '@nestjs/config';
 import { ServicesModule } from './services/services.module';
-
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/carvex'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.DATABASE_URI!),
+
     AuthModule,
     UsersModule,
     CarsModule,
-     ConfigModule,
     ReservationsModule,
     OffersModule,
     ServicesModule,
   ],
 })
 export class AppModule {}
-
-
